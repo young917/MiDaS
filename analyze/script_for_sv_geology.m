0f1862497f223e89f3f8293788b974ad3b67925c
MinYoung Choe <30820487+young917@users.noreply.github.com>
2024-03-15 16:46:08 +0900

    Update script_for_sv_geology.m

--- analyze/script_for_sv_geology.m
+++ analyze/script_for_sv_geology.m
@@ -1,6 +1,6 @@
 for portion_str={"0.10", "0.20", "0.30", "0.40", "0.50"};
     for repeat_str={"1", "2", "3"};
-        for algoname={"midas", "ns/global_deg_1.0000", "es/global_deg_min_0.0000", "ns/global_deg_0.0000", "tihs", "rw/rw_c_1", "ff/ff_c_0.51_0.20", "mgs/add_degree", "mgs/exchange_degree", "mgs/remove_degree", "mgs/add_avg", "mgs/exchange_avg", "mgs/remove_avg", "midas_grid_ablation", "maxdegree", "avgdegree", "midas_ns"};
+        for algoname={"midas", "ns/global_deg_1.0000", "es/global_deg_min_0.0000", "ns/global_deg_0.0000", "tihs", "rw/rw_c_1", "ff/ff_c_0.51_0.20", "hrw/noback", "hrw/skip", "mgs/add_degree", "mgs/exchange_degree", "mgs/remove_degree", "mgs/add_avg", "mgs/exchange_avg", "mgs/remove_avg", "midas_grid_ablation", "maxdegree", "avgdegree", "midas_ns"};
             fname = algoname{1} + '/coauth-MAG-Geology-full_' + portion_str{1} + '/' + repeat_str{1}
             [fileID_input, msg] = fopen('../Hypergraph_Sampling_cpp/results/'+ algoname{1} + '/coauth-MAG-Geology-full_' + portion_str{1} + '/' + repeat_str{1} + '/sampled_graph.txt', 'r');
             [fileID, msg] = fopen('input/' + algoname{1} + '/coauth-MAG-Geology-full_' + portion_str{1} + '/' + repeat_str{1} + '/icd_row.txt','r');
